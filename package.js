af9612d91f0474230dcecfee81ce36418d61881a
Jimmy Aupperlee <j.aup.gt@gmail.com>
2015-12-21 16:25:08 +0100

    Added meteor version

--- package.js
+++ package.js
@@ -7,6 +7,8 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+
   api.use('underscore');
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
