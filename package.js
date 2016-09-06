cec8c9ef92aa5bf3ad9929f85765a5bd1a2e463a
volodymyrtymets <volodymyrtymets@gmail.com>
2016-09-06 14:37:34 +0300

    increase version of depend package q42:microsoft

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('q42:microsoft@0.1.0', ['client', 'server']);
+  api.use('q42:microsoft@1.0.1', ['client', 'server']);
 
   api.addFiles(['client/login.js', 'client/login_button.css'], 'client');
   api.addFiles('lib/register.js');
